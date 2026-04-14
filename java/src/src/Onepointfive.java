19279cc2ec0ec9a8b0dd73ec8017d930a9896749
Administrator <Administrator@DESKTOP-15TFTUR>
2026-04-14 22:23:16 +0530

    commit my project

---
+++ java/src/src/Onepointfive.java
@@ -0,0 +1,22 @@
+package src;
+
+public class Onepointfive {
+
+	public static void main(String[] args) {
+		String firstname="Divya";
+		String lastname="Bharathi";
+		char gender='F';
+		int age = 20;
+		double weight =85.55;
+		
+		System.out.println("Person Details:\n");
+		System.out.println("--------------");
+		System.out.println("First Name:"+firstname);
+		System.out.println("Last Name:"+lastname);
+		System.out.println("gender:"+gender);
+		System.out.println("Age:"+age);
+		System.out.println("Weight:"+weight);
+
+	}
+
+}
