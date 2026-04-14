725d2de41589481fa1d7db1c1f8df99b2cb1ec73
chaitanyaa0023-sketch <chaitanyaa0023@gmail.com>
2026-04-14 23:59:54 +0530

    Add files via upload

---
+++ java/src/src/positive.java
@@ -0,0 +1,19 @@
+package src;
+import java.util.*;
+
+public class positive {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int num3 = sc.nextInt();
+		if(num3>0) {
+			System.out.println("Positive");
+		}
+		else {
+			System.out.println("Negitive");
+		}
+		
+
+	}
+
+}
