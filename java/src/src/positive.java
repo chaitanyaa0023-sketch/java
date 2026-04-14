9bc6a29b2aa3764d0e4b720feda41861d1d54554
Administrator <Administrator@DESKTOP-15TFTUR>
2026-04-14 23:15:50 +0530

    processss

--- java/src/src/positive.java
+++ java/src/src/positive.java
@@ -5,8 +5,8 @@ public class positive {
 
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-		int num1 = sc.nextInt();
-		if(num1>0) {
+		int num2 = sc.nextInt();
+		if(num2>0) {
 			System.out.println("Positive");
 		}
 		else {
