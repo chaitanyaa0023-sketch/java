5561c245aa7285ff19371ab33c75235c11ff1b6c
Administrator <Administrator@DESKTOP-15TFTUR>
2026-04-17 11:05:51 +0530

    commit phonenumber

---
+++ java/src/src/PhoneNumberMain.java
@@ -0,0 +1,19 @@
+
+package src;
+import src.Person;
+
+public class PhoneNumberMain {
+	
+	
+
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PhoneNumber a =  new PhoneNumber("Divya","Bharathi",'F',20,85.55,854635662L);
+		a.display();
+		
+
+}
+
+}
